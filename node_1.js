7639d79d92933bf5b36622fe3c02b37a1fe087c1
Bianca Bringel <bbringel@outlook.com>
2024-01-16 11:17:53 -0500

    1st Node exercise

---
+++ node_1.js
@@ -0,0 +1,14 @@
+/* shape-area.js */
+const PI = Math.PI;
+
+// Define and export circleArea() and squareArea() below
+
+function circleArea(r) {
+  return PI * r * r;
+}
+function squareArea(side) {
+  return side * side;
+}
+
+module.exports.circleArea = circleArea;
+module.exports.squareArea = squareArea;
