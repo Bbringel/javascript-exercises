bc51c1ee89308ec8567473806e6dad9cf8e6bdff
Bianca Bringel <bbringel@outlook.com>
2024-01-23 10:16:35 -0500

    Then, Catch, All & Race

--- promises.js
+++ promises.js
@@ -1,3 +1,5 @@
+//CodeCademy 1st exercise:
+
 const inventory = {
   sunglasses: 1900,
   pants: 1088,
@@ -21,3 +23,72 @@ const orderSunglasses = () => {
 const orderPromise = orderSunglasses();
 
 console.log(orderPromise);
+
+
+// Promises then & catch:
+
+let p = new Promise((resolve, reject) => {
+  let a = 1 + 1
+  if (a == 2) {
+    resolve('Sucess')'
+  } else {
+    reject('Failed')
+  }
+})
+
+p.then((message) => {
+  console.log('This is in the then ' + message)
+}).catch((message) => {
+  console.log('This is int he catch ' + message)
+})
+
+// Promises all & race:
+
+const cookLasagnaOne = new Promise ((resolve, reject) => {
+  resolve('Lasagna One Done')  
+})
+
+const cookLasagnaTwo = new Promise ((resolve, reject) => {
+  resolve('Lasagna Two Done')  
+})
+
+const cookLasagnaThree = new Promise ((resolve, reject) => {
+  resolve('Lasagna Three Done')  
+})
+
+// Everything printed in the console:
+Promise.all([
+    cookLasagnaOne,
+    cookLasagnaTwo,
+    cookLasagnaThree
+]).then((messages) => {
+  console.log(messages)
+})
+
+// The 1st one to be resolved printed in the console:
+Promise.race([
+    cookLasagnaOne,
+    cookLasagnaTwo,
+    cookLasagnaThree
+]).then((messages) => {
+  console.log(messages)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
